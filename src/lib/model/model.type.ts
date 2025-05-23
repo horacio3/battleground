0e63cc9b2f360e5af033c6cfd78ce8287460b477
jspaeth <jon.spaeth@caylent.com>
2025-05-23 12:08:15 -0500

    add claude 4

--- src/lib/model/model.type.ts
+++ src/lib/model/model.type.ts
@@ -32,6 +32,8 @@ export type TextModelId =
   | "anthropic.claude-3-5-sonnet-20240620-v1:0"
   | "anthropic.claude-3-5-sonnet-20241022-v2:0"
   | "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
+  | "us.anthropic.claude-sonnet-4-20250514-v1:0"
+  | "us.anthropic.claude-opus-4-20250514-v1:0"
   | "us.deepseek.r1-v1:0"
   | "cohere.command-text-v14"
   | "cohere.command-light-text-v14"
